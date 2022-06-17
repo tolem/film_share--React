ccf25bd87db519e5cfec7f09aca3020ce18e3919
Lami <olamide2olowoniyi@gmail.com>
2022-06-16 23:49:28 -0400

    adding starter code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useEffect } from 'react'
+// 47cdbda
+const Api_url = "http://www.omdbapi.com/?apikey=[47cdbda]&"
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
